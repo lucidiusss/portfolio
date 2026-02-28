9007aec146e46ecd1f90e37d21df86f2eaab19fd
Nick <cheapp78@gmail.com>
2026-02-28 15:54:06 +0300

    minor fixes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,12 @@
 import Header from "./Header";
 import { Outlet } from "react-router";
-import { HeroUIProvider } from "@heroui/system";
 
 const Layout = () => {
     return (
         <div>
-            <HeroUIProvider>
-                <Header />
-                <Outlet />
-                {/*<Player />*/}
-            </HeroUIProvider>
+            <Header />
+            <Outlet />
+            {/*<Player />*/}
         </div>
     );
 };
